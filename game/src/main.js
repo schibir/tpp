ed65ae9b3374e8f7e0f02bb7f63a19b1219f805d
Sergey Chibiryaev <zav.gamedev@gmail.com>
2017-12-06 16:21:18 +0300

    added babel, eslint, browserify, uglify

---
+++ game/src/main.js
@@ -0,0 +1,8 @@
+
+function main() {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = 100;
+    canvas.height = 100;
+}
+window.addEventListener("load", main);
