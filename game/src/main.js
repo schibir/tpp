fe599536ff9a81b77ce181402be8ec43392b1bfd
Sergey Chibiryaev <zav.gamedev@gmail.com>
2017-12-06 17:13:28 +0300

    refact

--- game/src/main.js
+++ game/src/main.js
@@ -1,7 +1,9 @@
 
+// Size of map 36x20
+
 function main() {
     const canvas = document.getElementById("canvas");
-    canvas.width = 100;
-    canvas.height = 100;
+    canvas.width = window.innerWidth - 200;
+    canvas.height = window.innerHeight - 200;
 }
 window.addEventListener("load", main);
