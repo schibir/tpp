2fb0b4bc138487ec6b445635146244afd41c1dd8
Sergey Chibiryaev <zav.gamedev@gmail.com>
2017-12-10 16:07:51 +0300

    added class tank

---
+++ game/src/tank.js
@@ -0,0 +1,21 @@
+
+import Entity from "./entity";
+
+export const TANK = {
+    TANK1: 0,
+    TANK2: 1,
+    SIMPLE: 2,
+    BMP: 3,
+    CANNON: 4,
+    STRONG: 5,
+    PANZER: 6,
+    RANDOM: 7,
+};
+
+export class Tank extends Entity {
+    constructor(cx, cy, type) {
+        super(cx, cy);
+        this.type = type;
+    }
+}
+
