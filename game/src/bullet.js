a5ee132e677d810f60db090d7050d2424133532a
schibir <zav.gamedev@gmail.com>
2017-12-15 12:55:24 +0300

    bullet born in properly place after shoot

--- game/src/bullet.js
+++ game/src/bullet.js
@@ -11,7 +11,11 @@ export const BULLET = {
 
 export class Bullet extends Entity {
     constructor(owner) {
-        super(owner.cx, owner.cy, 1, owner.angle, 0.1);
+        const sina = [-1, 0, 1, 0];
+        const cosa = [0, 1, 0, -1];
+        const dx = cosa[owner.angle];
+        const dy = sina[owner.angle];
+        super(owner.cx + dx, owner.cy + dy, 1, owner.angle, 0.1);
         this.owner = owner;
     }
     clear(level) {
