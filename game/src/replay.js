6e5bd34a63f26b7d54f1d6a4c1813406d0ca4675
Sergey Chibiryaev <zav.gamedev@gmail.com>
2018-08-12 12:13:41 +0300

    add mocView for calculate size of serialized replay

--- game/src/replay.js
+++ game/src/replay.js
@@ -2,6 +2,24 @@
 import { Random } from "./utils";
 import { ITEM, TANK } from "./global";
 
+class MocView {
+    constructor() {
+        this.currentOffset = 0;
+    }
+    addUint16() {
+        this.currentOffset += 2 * 8;
+    }
+    addUint32() {
+        this.currentOffset += 4 * 8;
+    }
+    addBits(bits) {
+        this.currentOffset += bits;
+    }
+    addBool() {
+        this.currentOffset++;
+    }
+}
+
 export default class Replay {
     constructor(level, tanks) {
         this.random_seed = Random.getSeed();
@@ -31,6 +49,29 @@ export default class Replay {
         });
     }
     save() {
-        console.log(`Save replay with seed = ${this.random_seed}`);
+        const mocView = new MocView();
+        mocView.addUint32(this.random_seed);
+        mocView.addBits(3, this.level);         // 0 - 5
+        mocView.addBits(4, this.difficulty);    // 0 - 15
+        mocView.addBits(4, this.life);          // 0 - inf, but 15 impossible
+        mocView.addBits(9, this.scores);        // max scores for levelup = 400
+        mocView.addUint16(this.total_scores);
+        mocView.addBool(this.items[ITEM.FIREBALL]);
+        mocView.addBool(this.items[ITEM.SPEED]);
+        mocView.addBool(this.items[ITEM.STAR]);
+        mocView.addBool(this.players[TANK.TANK1]);
+        mocView.addBool(this.players[TANK.TANK2]);
+        if (this.players[TANK.TANK1]) {
+            mocView.addBits(3, this.players[TANK.TANK1].weaponType);    // 0 - 7
+            mocView.addBits(3, this.players[TANK.TANK1].life);          // 1 - 6
+            mocView.addBool(this.players[TANK.TANK1].velocity > 2);     // super speed = 2.4
+        }
+        if (this.players[TANK.TANK2]) {
+            mocView.addBits(3, this.players[TANK.TANK2].weaponType);    // 0 - 7
+            mocView.addBits(3, this.players[TANK.TANK2].life);          // 1 - 6
+            mocView.addBool(this.players[TANK.TANK2].velocity > 2);     // super speed = 2.4
+        }
+
+        console.log(`Size replay = ${mocView.currentOffset} bits`);
     }
 }
